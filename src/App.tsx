eca3a27cd7ecaaf4fb6f93a33b68ef46dab58818
Eliad Badin <eliad.badin@moveo.co.il>
2025-12-21 14:52:17 +0200

    First setup - initialize Vite + React + TypeScript + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='p-6 text-2xl font-bold'>
+      Epicure
+    </div>
+  )
+}
+
+export default App
